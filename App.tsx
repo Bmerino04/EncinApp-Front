fb8e5d076ee92acda7e892ec99aaa84750dc8fd4
Armin Hucke <arminhucke@protonmail.com>
2025-05-26 02:16:34 -0400

    fix: fix routing (it doesn't work)

--- App.tsx
+++ App.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
-import { theme } from '@/theme/catppuccinLatte';
-import AnnouncementsScreen from './announcements';
-import AnnouncementDetailScreen from './AnnouncementDetail';
+import { theme } from './theme/catppuccinLatte';
+import HomeScreen from './screens/HomeScreen';
+import AnnouncementsScreen from './screens/AnnouncementsScreen';
+import AnnouncementDetailScreen from './screens/AnnouncementDetailScreen';
+import { RootStackParamList } from './navigation/types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function AppLayout() {
+export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="announcements" component={AnnouncementsScreen} />
           <Stack.Screen name="AnnouncementDetail" component={AnnouncementDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
   );
-}
+} 
