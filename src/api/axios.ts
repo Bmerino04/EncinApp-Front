3fb9c44e001d2124bf3b5a866bbce69227f604a7
Bmerino04 <bmerinomunoz0404@gmail.com>
2025-06-09 14:28:55 -0400

    fix: axios not communicating with mobile device

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import Constants from 'expo-constants';
 
-const API_URL = Constants.manifest?.extra?.apiUrl;
+const API_URL =
+  Constants.manifest?.extra?.apiUrl || // modo desarrollo
+  Constants.expoConfig?.extra?.apiUrl || // modo producción
+  'http://192.168.58.111:3000/api'; // fallback
+  
 
 export const api = axios.create({
   baseURL: API_URL,
