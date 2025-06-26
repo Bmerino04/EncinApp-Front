919d4f0c6004f21a903c4c6ab2511e3bcc0998d4
Bmerino04 <bmerinomunoz0404@gmail.com>
2025-06-26 19:26:54 -0400

    feat: se añade google services a app.config.js y se añade archivo eas.json

--- app.config.js
+++ app.config.js
@@ -7,9 +7,13 @@ export default {
     version: '1.0.0',
     extra: {
       apiUrl: process.env.API_URL || 'http://192.168.127.111:3000/api',
+      eas: {
+        projectId: 'b35caac4-09ee-4779-ba1e-a3a8f66f2f58',
+      },
     },
     android: {
       package: 'com.encinapp.dev',
+      googleServicesFile: './android/google-services.json',
     },
     ios: {
       bundleIdentifier: 'com.encinapp.dev',
